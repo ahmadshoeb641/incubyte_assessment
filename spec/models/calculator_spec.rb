afd4e6c1e05ef30a8728f770b1fa058dee2e9c3c
Shoeb Ahmad <ahmadshoeb641@gmail.com>
2024-04-16 17:47:35 +0530

    write rspec for empty string and single number

--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Calculator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.add' do
+    it 'returns 0 for an empty string' do
+      expect(Calculator.add("")).to eq(0)
+    end
+
+    it 'returns the number itself for a single number' do
+      expect(Calculator.add("1")).to eq(1)
+    end
+  end
 end
